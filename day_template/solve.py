4bfb16f04b11af185e7212baa2013579aa2f5867
Jeremy <jeremy.savor@gmail.com>
2019-12-08 12:49:34 +0100

    Add template for daily challenges

---
+++ day_template/solve.py
@@ -0,0 +1,26 @@
+"""
+Solving Day _, 2019 Advent of Code Challenge
+---
+
+"""
+
+import sys
+import os
+utils_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+sys.path.append(utils_path)
+from utils import get_input_text, write_solution
+
+if __name__ == "__main__":
+    day = 3
+    year = 2019
+    input_text = get_input_text(day, year)
+
+    ## Part 1 ##
+    print(f"Solution Day {day} (Part 1):")
+
+    #  write_solution(day, year,
+
+    ## Part 2 ##
+    #  print(f"Solution Day {day} (Part 2):")
+
+    #  write_solution(day, year,
